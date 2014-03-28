80c70acd80457c9259592340716fd1b9ee37c545
vincentdo <dodviet@sas.upenn.edu>
2014-03-28 00:03:26 -0400

    added function for extracting training set

--- src/util/extractor.py
+++ src/util/extractor.py
@@ -1,3 +1,26 @@
 import sys
+from collections import namedtuple
 
-lang = ['en', 'deu', 'ita', '']
+lang = ['en', 'deu', 'ita', 'spa', 'fra']
+
+file_suffix = '_wikipedia_2010_100K_sentences.txt'
+
+TRAIN_MAX = 30000
+
+test_output = open('test', 'w')
+dev_output = open('dev', 'w')
+
+for i in xrange(len(lang)):
+	input_file = open(lang + 'file_suffix')
+  output_file = open(lang + '_train', 'w')
+	for line in input_file:
+    (i, sentence) = line.strip().split()
+    if (i <= TRAIN_MAX):
+      # Extracting training set
+      output_file.write(sentence + '\n')
+    else
+      output_file.close()
+
+
+
+
