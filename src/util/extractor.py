ebd0624b84414683cb709fe0fb878e0ec66c5bb8
Vincent Do <do.vincent@live.com>
2014-03-29 05:15:12 -0400

    Finished extractor.py

--- src/util/extractor.py
+++ src/util/extractor.py
@@ -1,4 +1,5 @@
 import sys
+import random
 from collections import namedtuple
 
 lang = ['en', 'deu', 'ita', 'spa', 'fra']
@@ -7,8 +8,12 @@ file_suffix = '_wikipedia_2010_100K_sentences.txt'
 
 TRAIN_MAX = 30000
 
+random.seed()
+
 test_output = open('test', 'w')
 dev_output = open('dev', 'w')
+
+test_files = [test_output, dev_output]
 
 for i in xrange(len(lang)):
 	input_file = open(lang + 'file_suffix')
@@ -18,8 +23,19 @@ for i in xrange(len(lang)):
     if (i <= TRAIN_MAX):
       # Extracting training set
       output_file.write(sentence + '\n')
-    else
-      output_file.close()
+    else:
+      if not out_file.closed:
+        output_file.close()
+      else: 
+        start_bound = random.randint(0, len(line)-1)
+        end_bound = random.randint(0, len(line)-1)
+        f = random.choice(test_files)
+        f.write(str(sentence[start_bound:end_bound]))
+
+test_output.close()
+dev_output.close()
+
+
 
 
 
