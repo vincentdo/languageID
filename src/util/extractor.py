91ee7c9b72aecf66a13bcd8ae3da15bdc02915df
Your Name <do.vincent@live.com>
2014-04-03 01:10:26 -0400

    Fix extractor to extract training, dev, and test data

--- src/util/extractor.py
+++ src/util/extractor.py
@@ -2,41 +2,40 @@ import sys
 import random
 from collections import namedtuple
 
-lang = ['en', 'deu', 'ita', 'spa', 'fra']
+lang = ['eng', 'deu', 'ita', 'spa', 'fra']
 
-file_suffix = '_wikipedia_2010_100K_sentences.txt'
+file_suffix = '_wikipedia_2010_100K-sentences.txt'
+corpora_input = '../../data/corpora/'
+test_output = '../../data/test/'
+dev_output = '../../data/dev/'
+train_output = '../../data/train/'
 
 TRAIN_MAX = 30000
+SAMPLE_LENGTH = 40
 
 random.seed()
 
-test_output = open('test', 'w')
-dev_output = open('dev', 'w')
+test_output = open(test_output + 'test', 'w')
+dev_output = open(dev_output + 'dev', 'w')
 
 test_files = [test_output, dev_output]
 
 for i in xrange(len(lang)):
-	input_file = open(lang + 'file_suffix')
-  output_file = open(lang + '_train', 'w')
-	for line in input_file:
-    (i, sentence) = line.strip().split()
-    if (i <= TRAIN_MAX):
+  input_file = open(corpora_input + lang[i] + file_suffix)
+  output_file = open(train_output + lang[i] + '_train', 'w')
+  for line in input_file:
+    (j, sentence) = line.strip().split('\t')
+    if (int(j) <= TRAIN_MAX):
       # Extracting training set
       output_file.write(sentence + '\n')
     else:
-      if not out_file.closed:
+      if not output_file.closed:
         output_file.close()
       else: 
-        start_bound = random.randint(0, len(line)-1)
-        end_bound = random.randint(0, len(line)-1)
+        start_bound = random.randint(0, len(sentence))
+        end_bound = start_bound + random.randint(15,50)
         f = random.choice(test_files)
-        f.write(str(sentence[start_bound:end_bound]))
+        f.write(str(sentence[start_bound:end_bound]) + '\n')
 
 test_output.close()
 dev_output.close()
-
-
-
-
-
-
